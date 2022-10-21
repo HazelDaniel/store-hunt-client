15b459460f6c045d499fe14d6439a904a3f56d4b
HazelDaniel <hazeldaniel856@gmail.com>
2022-10-21 07:55:29 +0100

    correction:  day 13 without mobile phone or access to the internet. made the shop product more dynamic with each product having its own data . made the label more dynamic

--- src/components/shop-products-display/shop-products-display.component.jsx
+++ src/components/shop-products-display/shop-products-display.component.jsx
@@ -2,6 +2,7 @@ import { ShopProduct } from "../shop-product/shop-product.component";
 import { ShopProductsDisplayStyled, PaginationButtonsDivStyled, ShopPaginationButtonStyled, ShopLoadMoreButtonStyled } from "./shop-products-display.styles";
 
 const SHOP_PRODUCTS_DATA = {
+	
 	products: [
 		{
 			name: `NIKE N1`,
